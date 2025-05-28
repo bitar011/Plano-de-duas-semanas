d91260471952a9bd726ef1d1d7bb786897eb0fb2
bitar011 <victorbitarr@gmail.com>
2025-05-27 22:21:12 -0300

    Adiciona milissegundos ao cronômetro e atualiza a exibição para incluir frações de segundo

--- dia5 - Cronometro/script.js
+++ dia5 - Cronometro/script.js
@@ -1,20 +1,26 @@
 let seconds = 0;
 let timer = null;
+let milliseconds = 0;
 
 function updateDisplay(){
     const hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
     const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
     const secs = String(seconds % 60).padStart(2, '0');
-    document.getElementById('display').textContent = `${hrs}:${mins}:${secs}`;
+    const msecs = String(milliseconds / 10).padStart(2, '0'); 
+    document.getElementById('display').textContent = `${hrs}:${mins}:${secs}.${msecs}`;
 }
 
 document.getElementById('start').addEventListener('click', () => {
     if (timer) return;
 
-    timer = setInterval(() => {
-        seconds++;
-        updateDisplay();
-    }, 1000)
+  timer = setInterval(() => {
+    milliseconds += 100;
+    if (milliseconds >= 1000) {
+      seconds++;
+      milliseconds = 0;
+    }
+    updateDisplay();
+  }, 100);
 });
 
 document.getElementById("pause").addEventListener("click", () => {
@@ -26,5 +32,9 @@ document.getElementById("reset").addEventListener("click", () => {
     clearInterval(timer);
     timer = null;
     seconds = 0;
+    milliseconds = 0;
     updateDisplay();
 });
+
+
+
