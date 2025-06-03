8d813ef1b6055c2f62d46c484c883abd4c8d34b2
bitar011 <victorbitarr@gmail.com>
2025-06-02 22:04:52 -0300

    atualização e melhoramento da interface

--- dia6 - Menu Responsivo/script.js
+++ dia6 - Menu Responsivo/script.js
@@ -4,3 +4,9 @@ const navLinks = document.querySelector('.nav-links')
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle('active')
 })
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove('active')
+    })
+})
