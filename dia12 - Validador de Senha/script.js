49c55e852a85555225940c4f8972022822dfd273
bitar011 <victorbitarr@gmail.com>
2025-06-10 23:30:51 -0300

    adição de função para ver e ocultar senha, e adiciona demonstração de força da senha

--- dia12 - Validador de Senha/script.js
+++ dia12 - Validador de Senha/script.js
@@ -1,9 +1,17 @@
 const passwordInput = document.getElementById("password")
+const togglePassword = document.getElementById("toggle-password")
 
 const lenghtEl = document.getElementById("length")
 const uppercaseEl = document.getElementById("uppercase")
 const numberEl = document.getElementById("number")
 const specialEl = document.getElementById("special")
+const strengthEl = document.getElementById("strength");
+
+togglePassword.addEventListener("click", () => {
+    const type = passwordInput.type === "password" ? "text" : "password"
+    passwordInput.type = type
+    togglePassword.textContent = type === "password" ? "👁️" : "🙈"
+})
 
 passwordInput.addEventListener("input", () => {
     const password = passwordInput.value
@@ -18,12 +26,36 @@ passwordInput.addEventListener("input", () => {
     updateValidation(uppercaseEl, hasUppercase)
     updateValidation(numberEl, hasNumber)
     updateValidation(specialEl, hasSpecial)
+    updateStrength(hasLength, hasUppercase, hasNumber, hasSpecial)
 })
 
 function updateValidation(element, isValid){
-    if(isValid  ){
-        element.textContent = "🟢 " + element.textContent.slice(2)
-    } else {
-        element.textContent = "🔴" + element.textContent.slice(2)
+    element.textContent = (isValid ? "🟢" : "🔴") + element.textContent.slice(2)
+}
+
+function updateStrength(hasLength, hasUppercase, hasNumber, hasSpecial) {
+    const score = [hasLength, hasUppercase, hasNumber, hasSpecial].filter(Boolean).length
+
+    let strengthText = ""
+    let color = ""
+
+    switch(score){
+        case 0:
+        case 1:
+            strengthText = "Senha fraca"
+            color = "red"
+            break
+        case 2:
+            strengthText = "Média"
+            color = "orange"
+            break
+        case 3:
+        case 4:
+            strengthText = "Forte"
+            color = "green"
+            break
     }
+
+    strengthEl.textContent = `Força: ${strengthText}`
+    strengthEl.style.color = color
 }
